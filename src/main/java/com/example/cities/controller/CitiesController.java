910326cddb4759f10141244bcb96648669aa7b96
Jeff Holmes <jeffaholmes@gmail.com>
2015-07-03 13:08:29 -0700

    Added kill endpoint

--- src/main/java/com/example/cities/controller/CitiesController.java
+++ src/main/java/com/example/cities/controller/CitiesController.java
@@ -1,5 +1,7 @@
 package com.example.cities.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/cities")
 public class CitiesController {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(CitiesController.class);
+	
     @RequestMapping(value = "/version", method = RequestMethod.GET)
     public String version() {
         return System.getenv("VERSION");
     }
+    
+	@RequestMapping(value = "/kill", method = RequestMethod.GET)
+	public void kill() {
+		logger.warn("*** The system is shutting down. ***");
+		System.exit(0);
+	}
 }
